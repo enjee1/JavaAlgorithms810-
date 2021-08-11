2a99182847e43a664f6b0120b09314dead69e408
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-08-10 20:44:48 -0400

    Updated Main with assignment instructions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,49 @@
 package com.company;
 
+/*
+Create a new InteliJ project called ( JavaAlgorithms810 ) Complete the following algorithms where each algorithm is store in a new class and the solution is in a method called "run"
+
+
+
+        Spend the first 60 minutes of class working on the first two on your own. Then work on the last two as a group.
+
+
+        Create the classes with the names above the links
+
+
+
+
+        ArrayOfMultiples
+
+        https://edabit.com/challenge/rzpucPyoyEtXPo2BG
+
+
+
+        PerfectSquarePatch
+
+        https://edabit.com/challenge/7Tb7qMDQHtz3xpydd
+
+
+
+        Reverse the String (with a catch)
+
+        https://edabit.com/challenge/QdrLgb37ZZwYMKXwi
+
+
+
+        AtbashCipher
+
+        https://edabit.com/challenge/JmuM2cP5MvruRjr6c
+
+        (for this one create a cipher and decipher method)
+
+
+
+        To Submit:
+        Either submit a link to a GitHub page hosting your solutions OR upload the three classes you created. (Having them on GitHub is better)
+
+ */
+
 
 public class Main {
 
